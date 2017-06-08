871a29c1e91e317c1bf55831e588c28baaf6be37
Narin Luangrath <narin.luangrath@gmail.com>
2017-06-08 05:47:40 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,6 @@
 # code your #position_taken? method here!
+def position_taken?(board, pos)
+  entry = board[pos]
+  is_empty = (entry == " ") || (entry == "") || (entry == nil)
+  return !is_empty
+end
